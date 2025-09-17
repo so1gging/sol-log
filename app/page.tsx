8cca470f9c591392ed5eab2346ce45128a167b5d
김솔지 <so1gging@gmail.com>
2025-09-17 20:58:50 +0900

    개발 이력 작성

--- app/page.tsx
+++ app/page.tsx
@@ -36,11 +36,75 @@ export default function Home() {
           <h4>
             RA 개발팀
             <p className="info">2025.08 ~ 현재</p>
+            <p className="rule">
+              자사 데이터솔루션 RA 서비스를 개편하고 신규 기능 개발에 참여하고
+              있습니다.
+            </p>
           </h4>
-          <p>
-            React 및 SPA 아키텍처를 이용한 웹 애플리케이션 개발. 웹 접근성(A11y)
-            향상, 성능 최적화, 사용자 경험 개선 작업을 수행하였습니다.
-          </p>
+          <div className="history">
+            <ul>
+              <li>기존 CI/CD 최적화</li>
+              <li>서비스 안정성 및 레거시 시스템 개선</li>
+              <li>자체 BI 툴 필터 기능 설계·구현</li>
+            </ul>
+          </div>
+        </div>
+        <div className="job-item">
+          <h4>
+            RTB팀
+            <p className="info">2024.07 ~ 2025.08</p>
+            <p className="rule">
+              사내 업무 툴을 유지보수하고 차세대 프로토타입을 개발하였습니다.
+            </p>
+          </h4>
+          <div className="history">
+            <ul>
+              <li>팀 내 프론트엔드 기술 리딩하고 팀원 관리</li>
+              <li>API 캐시 및 상태 관리 최적화하여 성능 개선</li>
+              <li>프로토타입 기술 환경 및 아키텍처 설계</li>
+              <li>
+                Cypress ➝ Playwright E2E 테스트 Mirgration 및 테스트 시간 단축
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="job-item">
+          <h4>
+            Frontend 팀<p className="info">2021.12 ~ 2024.07</p>
+            <p className="rule">
+              자사 데이터솔루션 RA 서비스 PoC 부터 서비스 런칭까지 메인 개발자로
+              참여하였습니다.
+            </p>
+          </h4>
+          <div className="history">
+            <ul>
+              <li>
+                JavaScript기반 Static Map API 를 React 컴포넌트로 래핑해 사내 맵
+                라이브러리 구축
+              </li>
+              <li>API 캐시 및 상태 관리 최적화하여 성능 개선</li>
+              <li>Cypress 기반 E2E 테스트 자동화 구축</li>
+              <li>
+                소나큐브 및 데이터독 기반 에러 트래킹 및 정적 분석으로 코드 품질
+                향상 기여
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="job-item">
+          <h4>
+            솔루션개발 팀<p className="info">2020.09 ~ 2021.12</p>
+            <p className="rule">
+              사내 업무 툴 화면 및 API 개발 및 인프라 관리에 기여하였습니다.
+            </p>
+          </h4>
+          <div className="history">
+            <ul>
+              <li>기본 기능 화면 및 API 개발</li>
+              <li>사내 업무용 android 앱 유지보수</li>
+              <li>swiftUI로 사내 업무용 IOS 어플 프로토타입 개발</li>
+            </ul>
+          </div>
         </div>
       </section>
 
