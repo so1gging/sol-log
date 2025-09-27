0de9716fc17403a6fbce496e29d748053b197b05
Kim solji <65271008+so1gging@users.noreply.github.com>
2025-09-27 17:10:57 +0900

    기본 블로그 세팅 수정 (#1)
    * 새로운 블로그 포맷 적용
    
    * build error 제거

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { getPostList } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  const postList = await getPostList("record");
+
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <section>
+          <ul>
+            {postList.map((post) => (
+              <Link key={post.url} href={post.url}>
+                {Object.values(post).join(",")}
+              </Link>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,82 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import fs from "fs";
+import path from "path";
+import dayjs from "dayjs";
+import matter from "gray-matter";
+import readingTime from "reading-time";
+import { sync } from "glob";
+
+const BASE_PATH = "/src/blog";
+const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// MDX 파일 파싱 : abstract / detail 구분
+const parsePost = async (postPath: string): Promise<any> => {
+  const postAbstract = parsePostAbstract(postPath);
+  const postDetail = await parsePostDetail(postPath);
+  return { ...postAbstract, ...postDetail };
+};
+
+// MDX의 개요 파싱
+// url, cg path, cg name, slug
+export const parsePostAbstract = (postPath: string) => {
+  const normalizedPath = postPath.split(path.sep).join("/");
+  const filePath = normalizedPath
+    .slice(normalizedPath.indexOf(BASE_PATH))
+    .replace(`${BASE_PATH}/`, "")
+    .replace(".mdx", "");
+
+  const [categoryPath, slug] = filePath.split("/");
+  const url = `/blog/${categoryPath}/${slug}`;
+  const categoryPublicName = getCategoryPublicName(categoryPath);
+  return { url, categoryPath, categoryPublicName, slug };
+};
+
+// MDX Detail
+const parsePostDetail = async (postPath: string) => {
+  const file = fs.readFileSync(postPath, "utf8");
+  const { data, content } = matter(file);
+  const grayMatter = data;
+  const readingMinutes = Math.ceil(readingTime(content).minutes);
+  const dateString = dayjs(grayMatter.date)
+    .locale("ko")
+    .format("YYYY년 MM월 DD일");
+  return { ...grayMatter, dateString, content, readingMinutes };
+};
+
+// category folder name을 public name으로 변경 : dir_name -> Dir Name
+export const getCategoryPublicName = (dirPath: string) =>
+  dirPath
+    .split("_")
+    .map((token) => token[0].toUpperCase() + token.slice(1, token.length))
+    .join(" ");
+
+// post를 날짜 최신순으로 정렬
+const sortPostList = (PostList: any[]) => {
+  return PostList.sort((a, b) => (a.date > b.date ? -1 : 1));
+};
+
+// 모든 MDX 파일 조회
+export const getPostPaths = (category?: string) => {
+  const folder = category || "**";
+  const paths: string[] = sync(`${POSTS_PATH}/${folder}/**/*.mdx`);
+  return paths;
+};
+
+// 모든 포스트 목록 조회
+export const getPostList = async (category?: string): Promise<any[]> => {
+  const paths: string[] = getPostPaths(category);
+  const posts = await Promise.all(paths.map((postPath) => parsePost(postPath)));
+  return posts;
+};
+
+// post 상세 페이지 내용 조회
+export const getPostDetail = async (category: string, slug: string) => {
+  const filePath = `${POSTS_PATH}/${category}/${slug}/content.mdx`;
+  const detail = await parsePost(filePath);
+  return detail;
+};
