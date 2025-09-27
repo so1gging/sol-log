18bca7a81fcaef9c091e4ceeb0d37aad6a5f7a9f
김솔지 <so1gging@gmail.com>
2025-09-27 17:10:31 +0900

    build error 제거

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getPostDetail } from "@/lib/utils";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
+// @ts-expect-error - 타입 불일치 허용 (MDX 컴포넌트 타입 미지원)
 export default async function PostDetail({ params: { category, slug } }) {
   const post = await getPostDetail(category, slug);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 
 export default async function Home() {
   const postList = await getPostList("record");
-  console.log(postList);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -12,7 +11,9 @@ export default async function Home() {
         <section>
           <ul>
             {postList.map((post) => (
-              <Link href={post.url}>{Object.values(post).join(",")}</Link>
+              <Link key={post.url} href={post.url}>
+                {Object.values(post).join(",")}
+              </Link>
             ))}
           </ul>
         </section>
