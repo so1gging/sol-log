9c3e55b960497188f50bad9af8a22be9a5f5bd43
Kim solji <65271008+so1gging@users.noreply.github.com>
2025-09-29 22:13:41 +0900

    첫번째 게시글 생성 (#6)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,26 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex items-center w-full bg-white justify-between py-8">
-      <Link href="/">
-        <div className="flex items-center justify-between">
-          <div className="hidden h-6 text-sm sm:block">Kimsolzy</div>
-        </div>
-      </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
-          {/* 타이틀 메뉴들 */}
-        </div>
-        {/* 기타 옵션들 */}
+    <header className="w-full bg-white">
+      <div className="mx-auto flex h-18 max-w-screen-lg items-center justify-between px-4 sm:px-6">
+        {/* 로고 */}
+        <Link href="/" className="text-sm font-semibold">
+          Kimsolzy
+        </Link>
+
+        {/* 네비게이션 */}
+        <nav className="flex items-center space-x-6">
+          <Link
+            href="/blog"
+            className="text-sm text-gray-600 hover:text-stone-900">
+            Blog
+          </Link>
+          <Link
+            href="/about"
+            className="text-sm text-gray-600 hover:text-stone-900">
+            About
+          </Link>
+        </nav>
       </div>
     </header>
   );
