bf23d986ceb646eb5f6dd74b862aba9536a1e8d3
김솔지 <so1gging@gmail.com>
2025-09-29 21:23:28 +0900

    typography 적용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
 };
 
 export default nextConfig;
--- src/components/PostLayout/index.tsx
+++ src/components/PostLayout/index.tsx
@@ -27,13 +27,7 @@ const PostLayout = ({ post, children }: PostLayoutProps) => {
               </div>
             </div>
           </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0 dark:divide-gray-700">
-            <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
-              <div className="prose dark:prose-invert max-w-none pt-10 pb-8">
-                {children}
-              </div>
-            </div>
-          </div>
+          <div className="prose prose-slate max-w-fit">{children}</div>
         </div>
       </article>
     </section>
