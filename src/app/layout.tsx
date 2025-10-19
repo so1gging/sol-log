765b61f343661467ae60b22a2df818646b9e6efa
김솔지 <so1gging@gmail.com>
2025-10-19 11:31:11 +0900

    컴포넌트 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/components/layout/SideBar";
+import Tabs from "@/components/layout/Tabs";
 
 const notoSansKr = Noto_Sans_KR({
   weight: ["500", "400", "300"],
@@ -26,11 +28,9 @@ export default function RootLayout({
     <html lang="ko">
       <body className={bodyClass}>
         <main className="flex-1 flex h-full">
-          <aside className="w-64 border-r bg-gray-50">
-            <div>sideBar</div>
-          </aside>
+          <SideBar />
           <section className="flex-1 flex flex-col">
-            <div>content tab</div>
+            <Tabs />
             <div className="flex-1 overflow-y-auto p-4">{children}</div>
           </section>
         </main>
---
+++ src/components/layout/SideBar.tsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <aside className="w-64 border-r bg-gray-50">
+      <div>sideBar</div>
+    </aside>
+  );
+};
+
+export default SideBar;
