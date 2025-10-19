60684a9f597ef74addecb0c766c500e0e05d204e
김솔지 <so1gging@gmail.com>
2025-10-19 16:52:36 +0900

    Revert "style: 기본 스타일 적용 (#11)"
    This reverts commit 7e6af613

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
+import Header from "@/components/Header";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
-import SideBar from "@/components/layout/SideBar";
-import Tabs from "@/components/layout/Tabs";
 
 const notoSansKr = Noto_Sans_KR({
   weight: ["500", "400", "300"],
@@ -17,7 +16,8 @@ export const metadata: Metadata = {
   },
 };
 
-const bodyClass = `flex items-center text-stone-900 w-screen h-screen bg-white justify-between select-none rounded-2xl border-1 ${notoSansKr.className}`;
+const bodyClass = `flex items-center text-stone-900 w-full bg-white justify-between select-none ${notoSansKr.className}`;
+const sectionClass = "mx-auto w-full max-w-5xl px-4 sm:px-6";
 
 export default function RootLayout({
   children,
@@ -27,13 +27,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={bodyClass}>
-        <main className="flex-1 flex h-full">
-          <SideBar />
-          <section className="flex-1 flex flex-col">
-            <Tabs />
-            <div className="flex-1 overflow-y-auto p-4">{children}</div>
-          </section>
-        </main>
+        <section className={sectionClass}>
+          <Header />
+          <main className="mb-auto">{children}</main>
+        </section>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,5 +6,72 @@ import Link from "next/link";
 export default async function Home() {
   const postList = await getPostList("record");
 
-  return <div>ss</div>;
+  return (
+    <div className="divide-y divide-gray-200">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl leading-9 text-right font-light tracking-tight text-stone-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          어서오세요,
+          <br />
+          <span className="font-medium">김솔지블로그</span>입니다
+          <span className="font-bold text-[#ffae00]">.</span>
+          <br />
+        </h1>
+        <p className="text-base text-right leading-6 text-stone-500 mb-20">
+          방문해주셔서 감사합니다. <br /> 저는 이 공간에{" "}
+          <span className="underline decoration-wavy underline-offset-4">
+            제 기술적인 생각과 배운 점들을 기록
+          </span>
+          하고 있어요.
+        </p>
+        <ul className="divide-y divide-gray-200">
+          {!postList.length && "No posts found."}
+          {postList.map((post) => {
+            const { url, title, date, desc } = post;
+            return (
+              <Link key={url} href={url} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>
+                          {dayjs(date).format("YYYY-MM-DD")}{" "}
+                          {dayjs(date).locale("ko").format("dddd")}
+                        </time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl leading-4 font-bold tracking-tight">
+                            <p className="text-stone-900 dark:text-gray-100">
+                              {title}
+                            </p>
+                          </h2>
+                          {/* <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div> */}
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-stone-400">
+                          {desc}
+                        </div>
+                      </div>
+                      <div className="text-base leading-6 font-medium">
+                        <p
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read more: "${title}"`}>
+                          Read more &rarr;
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
